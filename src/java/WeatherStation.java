ab7b27443fd9db32fac68ba2b0bc0b8d347a2519
Doug Letterman <github@renderfast.com>
2009-02-24 01:21:13 -0800

    Added methods for great circle distance, bearing, and an unprojectLat that converts Mercator back to angular coordinates using decay.

--- src/java/WeatherStation.java
+++ src/java/WeatherStation.java
@@ -8,6 +8,7 @@ package com.renderfast.nwsclient;
 import java.util.*;
 import java.lang.Math.*;
 import net.rim.device.api.util.*; // for log()
+import net.rim.device.api.math.*;
 import java.io.*;
 import java.io.DataInputStream.*;
 
@@ -15,120 +16,186 @@ public class WeatherStation extends Object
 {
 	private static WeatherStation _root = null;
 	
+	private static final double nauticalMilePerLat = 3438.1598733552287;
+	private static final double nauticalMilePerLongitude = 3443.9307042677865;
+	private static final double milesPerNauticalMile = 1.15078;
+	
 	private static String _weatherStationFilePath = "/data/weather_stations.data";
 	
 	public String name;
-	public double lat;
-	public double lon;
+	public double lat_;
+	public double lon_;
 	public WeatherStation left;
 	public WeatherStation right;
 	
-	static double projectLat(double lat)
+	public static double projectLat(double lat)
 	{
 		return MathUtilities.log(Math.tan(lat) + (1.0/Math.cos(lat)));
 	}
 	
-	double pow(double x, double y)
-	{   
-		// pow method from java.net
-		//Convert the real power to a fractional form
-		int den = 1024; //declare the denominator to be 1024
-	
-		/*Conveniently 2^10=1024, so taking the square root 10
-		times will yield our estimate for n.  In our example
-		n^3=8^2    n^1024 = 8^683.*/
-	
-		int num = (int)(y*den); // declare numerator
-	
-		int iterations = 10;  /*declare the number of square root
-			iterations associated with our denominator, 1024.*/
-	
-		double n = Double.MAX_VALUE; /* we initialize our         
-			estimate, setting it to max*/
-	
-		while( n >= Double.MAX_VALUE && iterations > 1) {
-			/*  We try to set our estimate equal to the right
-			hand side of the equation (e.g., 8^2048).  If this
-			number is too large, we will have to rescale. */
-	
-			n = x;
-					
-			for( int i=1; i < num; i++ )n*=x;
-	
-			/*here, we handle the condition where our starting
-			point is too large*/
-			if( n >= Double.MAX_VALUE ) {
-				iterations--;  /*reduce the iterations by one*/
-							
-				den = (int)(den / 2);  /*redefine the denominator*/
-							
-				num = (int)(y*den); //redefine the numerator
-			}
-		}
-			
-		/*************************************************
-		** We now have an appropriately sized right-hand-side.
-		** Starting with this estimate for n, we proceed.
-		**************************************************/
-			
-		for( int i = 0; i < iterations; i++ )
-		{
-			n = Math.sqrt(n);
-		}
-			
-		// Return our estimate
-		return n;
-	}
-
-	/*
-	static double sinh(double z)
-	{
-		return 0.5 * (pow(Math.E, z) + pow(Math.E, -1*z));
-	}
-	
 	static double unprojectLat(double y)
 	{
-		return Math.atan(sinh(y));
+		// Decay method
+		double mult = 0.999;
+		double n = y*mult;
+		while (n > 1.0e-100) {
+			n *= mult;
+			double test = projectLat(n);
+			double check1 = y-test;
+			double check2 = test-y;
+			if (check1 > 0.0001 || check2 < 0.0001)
+				return n;
+			if (check2 > 0.75) {
+				mult = 0.99;
+			} else {
+				mult = 0.999;
+			}
+		}
+		return n;
 	}
-	*/
+	
+	public static double greatCircleDistance(double lat1, double lon1, double lat2, double lon2)
+	{
+		System.err.println("From "+String.valueOf(Math.toDegrees(lat1))+" "+String.valueOf(Math.toDegrees(lon1)));
+		System.err.println("To   "+String.valueOf(Math.toDegrees(lat2))+" "+String.valueOf(Math.toDegrees(lon2)));
+		double yDistance = (lat2 - lat1) * nauticalMilePerLat;
+		double xDistance = (Math.cos(lat1) + Math.cos(lat2) * (lon2 - lon1) * (nauticalMilePerLongitude / 2.));
+		return (Math.sqrt( yDistance * yDistance + xDistance * xDistance ) * milesPerNauticalMile);
+	}
+	
+	public static String bearing(double lat1, double lon1, double lat2, double lon2)
+	{
+		// sin(lon1-lon2)*cos(lat2)
+		//double x = Math.sin(lon1-lon2)*Math.cos(lat2); 
+		//         math.cos(lat1) * math.sin(lat2) *math.sin(lat2) - math.sin(lat1) * math.cos(lat2) * math.cos(lon1-lon2)
+        //double y = Math.cos(lat1) * Math.sin(lat2) * Math.sin(lat2) - Math.sin(lat1) * Math.cos(lat2) * Math.cos(lon1-lon2);
+		double x = lon2 - lon1;
+		double y = lat2 - lat1;
+		int res = Fixed32.atand2(Fixed32.tenThouToFP((int)(x*10000.)), Fixed32.tenThouToFP((int)(y*10000.)));
+		res = Fixed32.toRoundedInt(res);
+		res = (res + 360) % 360;
+		if (res > 337 || res <= 22) {
+			return "N";
+		} else if (res > 22 && res <= 67) {
+			return "NE";
+		} else if (res > 67 && res <= 112) {
+			return "E";
+		} else if (res > 112 && res <= 157) {
+			return "SE";
+		} else if (res > 157 && res <= 202) {
+			return "S";
+		} else if (res > 202 && res <= 247) {
+			return "SW";
+		} else if (res > 247 && res <= 292) {
+			return "W";
+		}
+		return "NW";
+	}
+	
+	public WeatherStation nearestNeighbor(double lat, double lon)
+	{
+		double[] point = { projectLat(Math.toRadians(lat)), Math.toRadians(lon) };
+		WeatherStation best = null;
+		Vector result = this.nearestNeighbor(point, 0, best, Double.MAX_VALUE);
+		WeatherStation station = (WeatherStation)result.elementAt(0);
+		return station;
+	}
+	
+	public static InputStream getList() throws IOException
+	{
+		// create a temp weather station to get the getResourceAsStream class method
+		WeatherStation tmp = new WeatherStation();
+		InputStream is = tmp.getClass().getResourceAsStream( _weatherStationFilePath );
+		return is;
+	}
+	
+	public static WeatherStation findNearest(double lat, double lon)
+	{
+		WeatherStation closest = new WeatherStation();
+		InputStream inputStream = null;
+		try {
+			if (_root == null) {
+				inputStream = getList(); 
+				DataInputStream din = new DataInputStream(inputStream);
+				int[] goUp = { 0 };
+				_root = _initTree(din, goUp);
+			}
+			closest = _root.nearestNeighbor(lat, lon);
+		} catch (IOException ioe) {
+			System.err.println("IO Error reading file");
+		} finally {
+			if (inputStream != null) {
+				try {
+					// Make sure we always close the weather stations list
+					inputStream.close();
+				} catch (Exception e) { }
+			}
+		}
+		return closest;
+	}
+	
+	
+	public static WeatherStation _initTree(final DataInputStream din, int[] goUp) throws IOException {
+		
+		// goUp codes: n > 0, n < 64 = left child, n levels up 
+		//             n > 64  = right child, n-64 levels up
+		
+		byte[] buff = new byte[4]; // weather station name
+		int bytes = din.read(buff);
+		if (bytes == -1)
+			return null;
+		WeatherStation sta = new WeatherStation();
+		sta.name = new String(buff);
+		sta.lat_ = din.readDouble();
+		sta.lon_ = din.readDouble();
+		
+		byte[] buff2 = new byte[1]; // weather station name
+		bytes = din.read(buff2);
+		if (bytes == -1)
+			return sta; // we're done
+		
+		goUp[0] = (int)buff2[0];
+		
+		if (goUp[0] == 0) {
+			sta.left = _initTree(din, goUp);
+		}
+		
+		if (goUp[0] == 0x40) {
+			sta.right = _initTree(din, goUp);
+		}
+		
+		goUp[0]--;
+		return sta;
+	}
 	
 	public WeatherStation() {
-		lat = 0.;
-		lon = 0.;
+		lat_ = 0.;
+		lon_ = 0.;
 		left = null;
 		right = null;
 	}
 	
 	public String getName() { return name; }
 	
+	public double getLat() { return unprojectLat(lat_); }
+	
+	public double getLon() { return lon_; }
+	
 	public double getCoord(int axis) {
 		if (axis == 0)
-			return lat;
-		return lon;
+			return lat_;
+		return lon_;
 	}
 	
 	public double distanceTo(double plat, double plon)
 	{
 		// return squared distance
-		return ((lat - plat)*(lat - plat) + (lon - plon)*(lon - plon));
-	}
-	
-	public String nearestNeighbor(double lat, double lon)
-	{
-		double[] point = { projectLat(Math.toRadians(lat)), Math.toRadians(lon) };
-		WeatherStation best = null;
-		Vector result = this.nearestNeighbor(point, 0, best, Double.MAX_VALUE);
-		WeatherStation station = (WeatherStation)result.elementAt(0);
-		Double bestDist = (Double)result.elementAt(1);
-		System.err.println(station.getName()+" "+String.valueOf(station.getCoord(0))+
-			" "+String.valueOf(station.getCoord(1))+" dist: "+bestDist.toString());
-		return station.getName();
+		return ((lat_ - plat)*(lat_ - plat) + (lon_ - plon)*(lon_ - plon));
 	}
 	
 	private Vector nearestNeighbor(double[] point, int depth, 
 									WeatherStation currentBest, double bestDist) 
 	{
-		System.err.println(this.getName());
 		int axis = depth % 2;
 		depth += 1;
 		WeatherStation child, otherChild;
@@ -188,71 +255,6 @@ public class WeatherStation extends Object
 		ret.addElement(currentBest);
 		ret.addElement(new Double(bestDist));
 		return ret;
-	}
-	
-	public InputStream getList() throws IOException
-	{
-		InputStream is = getClass().getResourceAsStream( _weatherStationFilePath );
-		return is;
-	}
-	
-	public static String findNearest(double lat, double lon)
-	{
-		InputStream inputStream = null;
-		try {
-			if (_root == null) {
-				WeatherStation tmp = new WeatherStation();
-				inputStream = tmp.getList(); 
-				DataInputStream din = new DataInputStream(inputStream);
-				int[] goUp = { 0 };
-				_root = _initTree(din, goUp);
-			}
-			return _root.nearestNeighbor(lat, lon);
-		} catch (IOException ioe) {
-			System.err.println("IO Error reading file");
-		} finally {
-			if (inputStream != null) {
-				try {
-					// Make sure we always close the weather stations list
-					inputStream.close();
-				} catch (Exception e) { }
-			}
-		}
-		return "";
-	}
-	
-	
-	public static WeatherStation _initTree(final DataInputStream din, int[] goUp) throws IOException {
-		
-		// goUp codes: n > 0, n < 64 = left child, n levels up 
-		//             n > 64  = right child, n-64 levels up
-		
-		byte[] buff = new byte[4]; // weather station name
-		int bytes = din.read(buff);
-		if (bytes == -1)
-			return null;
-		WeatherStation sta = new WeatherStation();
-		sta.name = new String(buff);
-		sta.lat = din.readDouble();
-		sta.lon = din.readDouble();
-		
-		byte[] buff2 = new byte[1]; // weather station name
-		bytes = din.read(buff2);
-		if (bytes == -1)
-			return sta; // we're done
-		
-		goUp[0] = (int)buff2[0];
-		
-		if (goUp[0] == 0) {
-			sta.left = _initTree(din, goUp);
-		}
-		
-		if (goUp[0] == 0x40) {
-			sta.right = _initTree(din, goUp);
-		}
-		
-		goUp[0]--;
-		return sta;
 	}
 	
 }
